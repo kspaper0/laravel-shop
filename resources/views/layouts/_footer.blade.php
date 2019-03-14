df8f1805d4f732cc806e2d1d0058ff8fc4410cba
kspaper_0 <15380324@qq.com>
2019-03-14 09:20:13 +0000

    basic layouts

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            Developed by <a href="http://www.baidu.com" target="_blank">Sean Kang </a> 
+        </p>
+
+        <p class="pull-right"><a href="mailto:sean@test.com">Contact Me</a></p>
+    </div>
+</footer>
