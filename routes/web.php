137ad0725290c25bc9c99e917c4cf6dd31f439e0
kspaper_0 <15380324@qq.com>
2019-03-21 11:28:50 +0000

    User - Product List

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@
 //     return view('welcome');
 // });
 
-Route::get('/', 'PagesController@root')->name('root');
+//Route::get('/', 'PagesController@root')->name('root');
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
 
 Route::group(['middleware' => 'auth'], function() {
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
