c36621363cc1e41db80a400711f5fbac1077d14d
kspaper_0 <15380324@qq.com>
2019-03-29 14:59:58 +0000

    Packaging Business Logic Codes

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,56 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\AddCartRequest;
-use App\Models\CartItem;
-use App\Models\ProductSku;
 use Illuminate\Http\Request;
+use App\Http\Requests\AddCartRequest;
+use App\Models\ProductSku;
+use App\Services\CartService;
 
 class CartController extends Controller
 {
-    public function add(AddCartRequest $request)
+    protected $cartService;
+
+    // 利用 Laravel 的自动解析功能注入 CartService 类
+    public function __construct(CartService $cartService)
     {
-        $user   = $request->user();
-        $skuId  = $request->input('sku_id');
-        $amount = $request->input('amount');
-
-
-        // 从数据库中查询该商品是否已经在购物车中
-        if ($cart = $user->cartItems()->where('product_sku_id', $skuId)->first()) {
-
-            // 如果存在则直接叠加商品数量
-            $cart->update([
-                'amount' => $cart->amount + $amount,
-            ]);
-        } else {
-
-            // 否则创建一个新的购物车记录
-            $cart = new CartItem(['amount' => $amount]);
-            $cart->user()->associate($user);
-            $cart->productSku()->associate($skuId);
-            $cart->save();
-        }
-
-        return [];
+        $this->cartService = $cartService;
     }
 
     public function index(Request $request)
     {
-        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
-        // with(['productSku.product']) 用来预加载购物车里的商品和 SKU 信息
-        // 使用了预加载之后
-        // 相当于 select * from product_skus where id in (cart_items)
-        // Laravel 还支持通过 . 的方式加载多层级的关联关系
-        // 这里我们就通过 . 提前加载了与商品 SKU 关联的商品
-
+        $cartItems = $this->cartService->get();
         $addresses = $request->user()->addresses()->orderBy('last_used_at', 'desc')->get();
 
         return view('cart.index', ['cartItems' => $cartItems, 'addresses' => $addresses]);
     }
 
+    public function add(AddCartRequest $request)
+    {
+        $this->cartService->add($request->input('sku_id'), $request->input('amount'));
+
+        return [];
+    }
+
     public function remove(ProductSku $sku, Request $request)
     {
-        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
+        $this->cartService->remove($sku->id);
 
         return [];
     }
