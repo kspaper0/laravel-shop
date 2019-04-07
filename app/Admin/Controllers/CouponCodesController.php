d7fd4b23929bf1f05a281334b0896141e8fde117
kspaper_0 <15380324@qq.com>
2019-04-07 13:59:25 +0000

    Admin - CUD Coupon

--- app/Admin/Controllers/CouponCodesController.php
+++ app/Admin/Controllers/CouponCodesController.php
@@ -49,13 +49,12 @@ class CouponCodesController extends Controller
      * @param Content $content
      * @return Content
      */
-    // public function edit($id, Content $content)
-    // {
-    //     return $content
-    //         ->header('Edit')
-    //         ->description('description')
-    //         ->body($this->form()->edit($id));
-    // }
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header('Edit Coupon')
+            ->body($this->form()->edit($id));
+    }
 
     /**
      * Create interface.
@@ -63,13 +62,12 @@ class CouponCodesController extends Controller
      * @param Content $content
      * @return Content
      */
-    // public function create(Content $content)
-    // {
-    //     return $content
-    //         ->header('Create')
-    //         ->description('description')
-    //         ->body($this->form());
-    // }
+    public function create(Content $content)
+    {
+        return $content
+            ->header('Publish Coupon')
+            ->body($this->form());
+    }
 
     /**
      * Make a grid builder.
@@ -147,16 +145,45 @@ class CouponCodesController extends Controller
     {
         $form = new Form(new CouponCode);
 
-        $form->text('name', 'Name');
-        $form->text('code', 'Code');
-        $form->text('type', 'Type');
-        $form->decimal('value', 'Value');
-        $form->number('total', 'Total');
-        $form->number('used', 'Used');
-        $form->decimal('min_amount', 'Min amount');
-        $form->datetime('not_before', 'Not before')->default(date('Y-m-d H:i:s'));
-        $form->datetime('not_after', 'Not after')->default(date('Y-m-d H:i:s'));
-        $form->switch('enabled', 'Enabled');
+        $form->display('id', 'ID');
+        $form->text('name', 'Name')->rules('required');
+        $form->text('code', 'Code')->rules(function($form) {
+            // 如果 $form->model()->id 不为空，代表是编辑操作
+            if ($id = $form->model()->id) {
+                return 'nullable|unique:coupon_codes,code,'.$id.',id';
+                // code 在 couple_codes 这个表中是 唯一的
+                // 可以保留原来的值 code
+                // 在这个模型的id 是否为当前要更改的 id
+            } else {
+                return 'nullable|unique:coupon_codes';
+            }
+        });
+        $form->radio('type', 'Type')->options(CouponCode::$typeMap)->rules('required');
+        $form->text('value', 'Value')->rules(function ($form) {
+            if ($form->model()->type === CouponCode::TYPE_PERCENT) {
+                // 如果选择了百分比折扣类型，那么折扣范围只能是 1 ~ 99
+                return 'required|numeric|between:1,99';
+            } else {
+                // 否则只要大等于 0.01 即可
+                return 'required|numeric|min:0.01';
+            }
+        });
+        $form->text('total', 'Total')->rules('required|numeric|min:0');
+        $form->text('min_amount', 'Min Amount')->rules('required|numeric|min:0');
+
+        $form->datetime('not_before', 'Begin at');
+        $form->datetime('not_after', 'End at');
+
+        $form->radio('enabled', 'Enable')->options(['1' => 'Yes', '0' => 'No']);
+
+        $form->saving(function (Form $form) {
+            if (!$form->code) {
+                $form->code = CouponCode::findAvailableCode();
+            }
+        });
+        //$form->datetime('not_before', 'Not before')->default(date('Y-m-d H:i:s'));
+        //$form->datetime('not_after', 'Not after')->default(date('Y-m-d H:i:s'));
+        //$form->switch('enabled', 'Enabled');
 
         return $form;
     }
