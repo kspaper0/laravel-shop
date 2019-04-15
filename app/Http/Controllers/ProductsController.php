9bc81b5fc8bd9a253a15e4895e30cd7b1fd3a71e
kspaper_0 <15380324@qq.com>
2019-04-15 07:10:23 +0000

    Navi Buttons - Category in header.php

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -7,9 +7,12 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Exceptions\InvalidRequestException;
 use App\Models\OrderItem;
+//use App\Services\CategoryService;
 
 class ProductsController extends Controller
 {
+    // 使用 Laravel 的依赖注入，自动创建 $categoryService 对象
+    // , CategoryService $categoryService
     public function index(Request $request)
     {
         // 创建一个查询构造器
@@ -69,6 +72,8 @@ class ProductsController extends Controller
             ],
             // 等价于 isset($category) ? $category : null
             'category' => $category ?? null, 
+            // 将类目树传递给模板文件
+            //'categoryTree' => $categoryService->getCategoryTree(),
         ]);
     }
 
