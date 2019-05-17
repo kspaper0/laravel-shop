7efa53c70315e7405d1f7a314df65761af11425c
kspaper_0 <15380324@qq.com>
2019-05-17 09:23:18 +0000

    Recommend Similar Products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -78,6 +78,11 @@ class Product extends Model
             });
     }
 
+    public function scopeByIds($query, $ids)
+    {
+        return $query->whereIn('id', $ids)->orderByRaw(sprintf("FIND_IN_SET(id, '%s')", join(',', $ids)));
+    }
+
     public function toESArray()
     {
         // 只取出需要的字段
